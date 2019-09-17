b9069a45f2bb350b537a13038fe0ef3e8004e49e
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2019-09-17 12:05:16 +0200

    Introduce lipo pre build

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,12 @@
 
 INFO_PLIST=$APPCENTER_SOURCE_DIRECTORY/ios/$APPCENTER_XCODE_SCHEME/Info.plist
 if [[ -e "$INFO_PLIST" ]]; then
+    echo "Stripping unwanted MendixNative (i386, x86_64) archs"
+    LIB_PATH=./ios/MendixNative/libMendix.a
+    lipo -remove x86_64 -output $LIB_PATH $LIB_PATH
+    lipo -remove i386 -output $LIB_PATH $LIB_PATH
+    lipo -info $LIB_PATH
+
     echo "Updating Info.plist with code push key"
     plutil -replace "CodePushKey" -string $CODE_PUSH_KEY $INFO_PLIST
 
